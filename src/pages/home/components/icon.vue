ca0935ea675f7f814ceee0daffe0904746e32764
nathanlu0902 <nathanlu0902@outlook.com>
2020-10-19 21:41:55 +0800

    icons

--- src/pages/home/components/icon.vue
+++ src/pages/home/components/icon.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="icons">
-  <swiper :options="swiperOptions">
-    <swiper-slide>
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="(page, index) of pages" :key="index">
       <div class="icon" 
-      v-for="item of iconList"
+      v-for="item of page"
       :key="item.id">
         <div class='icon-image'>
-            <img class='image-content' :src="item.url"/>
+            <img class='image-content' :src='item.url'/>
         </div>
         <p class="icon-desc">{{item.desc}}</p>
       </div>
@@ -28,50 +28,82 @@ export default {
       }, {
         id: '0002',
         url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
-        desc: 'first'
+        desc: '2'
       }, {
         id: '0003',
         url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
-        desc: 'first'
+        desc: '3'
+      }, {
+        id: '0004',
+        url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
+        desc: '4'
+      }, {
+        id: '0005',
+        url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
+        desc: '5'
+      }, {
+        id: '0006',
+        url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
+        desc: '6'
+      }, {
+        id: '0007',
+        url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
+        desc: '7'
+      }, {
+        id: '0008',
+        url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
+        desc: '8'
+      }, {
+        id: '0009',
+        url: 'https://tse1-mm.cn.bing.net/th/id/OIP.uvHQgVHPPycsGwKWQFVDCAHaHN?w=225&h=220&c=7&o=5&pid=1.7',
+        desc: '9'
       }]
     }
+  },
+    computed: {
+      pages () {
+        const pages=[]
+        this.iconList.forEach((item,index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        return pages
+    }
+    }
   }
-}
+
 </script>
 
-<style scoped>
+<style scoped> 
 .icons {
-  display: flex;
-  display: -webkit-flex;
   height: 0;
   padding-bottom: 50%;
-  flex-flow: row wrap;
-  overflow: hidden;
-  background-color: green;
 }
-div.icon {
+.icon {
   position: relative;
+  float: left;
   width: 25%;
   height: 0;
   padding-bottom: 25%;
-  box-sizing: border-box;
-  overflow: hidden;
-  background-color: blue;
 }
-div.icon-image {
+.icon-image {
   position: absolute;
+  box-sizing: border-box;
   top: .2rem;
   bottom: .44rem;
   right: 0;
   left: 0;
-  overflow: hidden;
+  padding: .1rem;
 }
-img.image-content {
-  position: block;
-  margin: 0 atuo;
-  height: 80%;
+.image-content {
+  display: block;
+  margin: 0 auto;
+  height: 100%;
 }
-p.icon-desc {
+.icon-desc {
   position: absolute;
   bottom: 0;
   left: 0;
@@ -79,5 +111,8 @@ p.icon-desc {
   line-height: .44rem;
   height: .44rem;
   text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 </style>
